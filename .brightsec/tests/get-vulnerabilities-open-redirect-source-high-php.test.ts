33f5c97d9900817589de12eff6aae78bc4a1ab7f
Alexander <lsndrme@ya.ru>
2026-01-09 00:23:04 +0700

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/get-vulnerabilities-open-redirect-source-high-php.test.ts
@@ -0,0 +1,42 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('GET /vulnerabilities/open_redirect/source/high.php', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['unvalidated_redirect', 'xss'],
+      attackParamLocations: [AttackParamLocation.QUERY],
+      starMetadata: {
+        code_source: 'lsndr/DVWA:master',
+        databases: ['MySQL'],
+        user_roles: {
+          roles: ['admin', 'regular_user']
+        }
+      },
+      poolSize: +process.env.SECTESTER_SCAN_POOL_SIZE || undefined
+    })
+    .setFailFast(false)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.GET,
+      url: `${baseUrl}/vulnerabilities/open_redirect/source/high.php?redirect=info.php`,
+      headers: { 'Content-Type': 'text/html' }
+    });
+});
---
+++ .brightsec/tests/post-vulnerabilities-brute-index-php.test.ts
@@ -0,0 +1,48 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('POST /vulnerabilities/brute/index.php', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['csrf', 'xss', 'sqli', 'brute_force'],
+      attackParamLocations: [AttackParamLocation.BODY],
+      starMetadata: {
+        code_source: 'lsndr/DVWA:master',
+        databases: ['MySQL'],
+        user_roles: {
+          roles: ['admin', 'regular_user']
+        }
+      },
+      poolSize: +process.env.SECTESTER_SCAN_POOL_SIZE || undefined
+    })
+    .setFailFast(false)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.POST,
+      url: `${baseUrl}/vulnerabilities/brute/index.php`,
+      body: {
+        username: 'admin',
+        password: 'password',
+        user_token: 'exampleToken',
+        Login: 'Login'
+      },
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    });
+});
